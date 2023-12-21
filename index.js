0c0e8540b0d0a19c622972b0ef31480c90ae32d1
lingeaniket <linge.aniket.10@gmail.com>
2023-12-21 18:53:27 +0530

    index updated

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ const progressBar = document.getElementById("progressBar");
 const songDuration = document.getElementById("song-duration");
 const mainContainer = document.getElementById("main-container");
 const musicPlayer = document.getElementById("music-player-main");
-const songCurrentTime = document.getElementById("song-current-time");
 const mainMusicPlayer = document.getElementById("music-player-div");
+const songCurrentTime = document.getElementById("song-current-time");
+
+const likedData = localStorage.getItem("liked-data")
+    ? JSON.parse(localStorage.getItem("liked-data"))
+    : { song: [], playlist: [], artist: [], album: [] };
 
 const musicPlayerData = {
     currentSong: 0,
@@ -17,19 +21,22 @@ const musicPlayerData = {
 };
 
 const loadSongs = async () => {
-    // mainMusicPlayer.classList.remove("not-playing");
     const list = await axios.get("https://saavn.me/modules?language=hindi,english,tamil,telugu");
     const homeData = list.data.data;
 
     for (let key in homeData) {
         const catMainTitle = document.createElement("div");
         catMainTitle.classList.add("cat-main-title");
+
         const h2Title = document.createElement("h2");
         h2Title.innerText = `top ${key}`;
+
         catMainTitle.append(h2Title);
+        mainContainer.append(catMainTitle);
 
         const cat01 = document.createElement("div");
         cat01.classList.add("cat-01");
+
         if (Array.isArray(homeData[key])) {
             createListItems(homeData[key], cat01);
         } else {
@@ -39,7 +46,6 @@ const loadSongs = async () => {
             }
         }
 
-        mainContainer.append(catMainTitle);
         mainContainer.append(cat01);
     }
 };
@@ -53,14 +59,72 @@ function createListItems(data, cat01, type) {
 
         const mainDiv = document.createElement("div");
 
-        mainDiv.onclick = playCategory.bind(list);
-
         const listImage = document.createElement("div");
         listImage.classList.add("listImage");
 
         const listimg = document.createElement("img");
         listimg.src = Array.isArray(list.image) ? list.image[2].link : list.image;
         listImage.append(listimg);
+
+        mainDiv.append(listImage);
+
+        const listBtns = document.createElement("div");
+        listBtns.className = "listBtns";
+
+        listBtns.onclick = openDetails.bind(list);
+
+        const listHov = document.createElement("div");
+        listHov.className = "listhov";
+
+        const listabs = document.createElement("div");
+        listabs.className = "listabs";
+
+        const listabs2 = document.createElement("div");
+        listabs2.className = "listabs listhovflex";
+
+        const listplaybtn = document.createElement("div");
+        listplaybtn.className = "listplaybtn";
+        listplaybtn.onclick = playCategory.bind(list);
+
+        listabs2.append(listplaybtn);
+        listabs.append(listabs2);
+
+        const listabs3 = document.createElement("div");
+        listabs3.className = "listabs listhovflex";
+        listabs3.style.pointerEvents = "none";
+
+        const listi1 = document.createElement("i");
+        listi1.className = "fa-solid fa-play fa-sm";
+        listi1.style.color = "white";
+
+        listabs3.append(listi1);
+        listabs.append(listabs3);
+        listHov.append(listabs);
+
+        const div1 = document.createElement("div");
+
+        const i1 = document.createElement("i");
+        i1.onclick = handleLike.bind(list);
+
+        if (likedData[list.type].some((val) => val === list.id)) {
+            i1.className = "fa-solid fa-heart fa-lg";
+        } else {
+            i1.className = "fa-regular fa-heart fa-lg";
+        }
+
+        div1.appendChild(i1);
+        listHov.append(div1);
+
+        const div2 = document.createElement("div");
+
+        const i2 = document.createElement("i");
+        i2.style.color = "white";
+        i2.className = "fa-solid fa-ellipsis fa-lg";
+
+        div2.appendChild(i2);
+        listHov.append(div2);
+        listBtns.append(listHov);
+        listImage.append(listBtns);
 
         const listTitle = document.createElement("div");
         listTitle.classList.add("list-title");
@@ -72,6 +136,8 @@ function createListItems(data, cat01, type) {
                 : list.type === "album" || list.type === "song"
                 ? list.name.replace(/&quot;/g, '"')
                 : list.title.replace(/&quot;/g, '"');
+
+        listTitle.append(titleh4);
 
         const titleP = document.createElement("p");
         titleP.innerText =
@@ -85,10 +151,8 @@ function createListItems(data, cat01, type) {
                 ? convertArtistToString(list.primaryArtists)
                 : list.subtitle;
 
-        listTitle.append(titleh4);
         listTitle.append(titleP);
 
-        mainDiv.append(listImage);
         mainDiv.append(listTitle);
         listMain.append(mainDiv);
         cat01.append(listMain);
@@ -98,7 +162,7 @@ function createListItems(data, cat01, type) {
 const updateContent = () => {
     const content = document.getElementById("main-container");
     content.innerHTML = "";
-    // console.log(window.location.pathname)
+
     if (window.location.pathname.includes("/search")) {
         loadSearch();
     } else if (window.location.pathname.includes("/get-details")) {
@@ -107,8 +171,6 @@ const updateContent = () => {
         loadSongs();
     }
 };
-
-
 
 const handleHomeRoute = () => {
     window.history.pushState({}, "", "/");
@@ -126,6 +188,28 @@ window.addEventListener("popstate", function () {
 
 window.onload = () => {
     updateContent();
-    // loadSongs();
-    // loadSearch();
 };
+
+async function handleLike(event) {
+    const { type, id } = this;
+
+    event.stopPropagation();
+
+    const classList = event.target.classList;
+
+    if (classList.contains("fa-solid")) {
+        event.target.classList.remove("fa-solid");
+        event.target.classList.add("fa-regular");
+
+        const index = likedData[type].indexOf(id);
+        likedData[type].splice(index, 1);
+
+        localStorage.setItem("liked-data", JSON.stringify(likedData));
+    } else {
+        event.target.classList.add("fa-solid");
+        event.target.classList.remove("fa-regular");
+        likedData[type].push(id);
+
+        localStorage.setItem("liked-data", JSON.stringify(likedData));
+    }
+}
