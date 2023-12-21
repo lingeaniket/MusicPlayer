a8a2238b180592a2097219210f66eb62db5d1a67
lingeaniket <linge.aniket.10@gmail.com>
2023-12-21 18:56:11 +0530

    search updated

--- SearchFunctions/searchFunctions.js
+++ SearchFunctions/searchFunctions.js
@@ -23,6 +23,7 @@ async function loadSearch() {
 
     search04_01.append(i1);
     search03.append(search04_01);
+
     const search04_02 = document.createElement("button");
     search04_02.classList.add("search04");
 
@@ -31,6 +32,7 @@ async function loadSearch() {
 
     search04_02.append(i2);
     search03.append(search04_02);
+    search02.append(search03);
 
     const search05 = document.createElement("div");
     search05.classList.add("search05");
@@ -41,6 +43,8 @@ async function loadSearch() {
     searchInput.placeholder = "What do you want to listen to?";
     searchInput.oninput = handleInput;
 
+    search05.append(searchInput);
+
     const searchicon = document.createElement("div");
     searchicon.classList.add("search-icon-div");
 
@@ -49,16 +53,14 @@ async function loadSearch() {
     i3.style.color = "#ffffff";
 
     searchicon.append(i3);
-
-    search05.append(searchInput);
     search05.append(searchicon);
+    search02.append(search05);
 
     const search06 = document.createElement("div");
     search06.classList.add("search06");
 
-    search02.append(search03);
-    search02.append(search05);
     search02.append(search06);
+    search01.append(search02);
 
     const search07 = document.createElement("div");
     search07.classList.add("search07");
@@ -69,10 +71,7 @@ async function loadSearch() {
     searchResultsDiv = search08;
 
     search07.append(search08);
-
-    search01.append(search02);
     search01.append(search07);
-
     mainContainer.append(search01);
 
     await loadTopTrends();
@@ -86,7 +85,6 @@ async function handleInput(event) {
     if (searchkey.length > 0) {
         searchTimer = setTimeout(() => {
             handleSearchkey();
-            console.log(searchkey);
         }, 1000);
     } else {
         await loadTopTrends();
@@ -99,17 +97,22 @@ async function handleSearchkey() {
     const searchData = await axios.get(`https://saavn.me/search/all?query=${searchkey}`);
 
     const searchResults = searchData.data.data;
+    const topResult = searchResults.topQuery.results[0];
+    const songsData = searchResults.songs.results;
 
     const search09 = document.createElement("div");
-    const topResult = searchResults.topQuery.results[0];
     search09.className = "search09";
+
     const h2Div = document.createElement("div");
+
     const h2main = document.createElement("h2");
     h2main.innerText = "Top Results";
     h2Div.append(h2main);
+    search09.append(h2Div);
 
     const search10 = document.createElement("div");
     search10.className = "search10";
+
     const search11 = document.createElement("div");
     search11.className = "search11";
 
@@ -117,19 +120,25 @@ async function handleSearchkey() {
     search11_img.src = topResult.image[2].link;
 
     search11.append(search11_img);
+    search10.append(search11);
 
     const search06 = document.createElement("div");
     search06.className = "search06";
+
+    search10.append(search06);
+    search09.append(search10);
 
     const search12 = document.createElement("div");
     search12.className = "search12";
 
     const h312 = document.createElement("h3");
     h312.innerText = topResult.title;
+
+    search12.append(h312);
+
     const p12 = document.createElement("p");
     p12.innerText = topResult.description;
 
-    search12.append(h312);
     search12.append(p12);
     search06.append(search12);
 
@@ -139,18 +148,11 @@ async function handleSearchkey() {
     search13.innerText = "Play";
     search13.onclick = playCategory.bind(topResult);
 
-    search10.append(search11);
-    search10.append(search06);
     search10.append(search13);
-
-    search09.append(h2Div);
-    search09.append(search10);
     searchResultsDiv.append(search09);
 
     const search09_01 = document.createElement("div");
     search09_01.className = "search09";
-
-    const songsData = searchResults.songs.results;
 
     loadSearchSongs(search09_01, songsData);
 
@@ -163,7 +165,6 @@ async function handleSearchkey() {
 
 async function loadTopTrends() {
     const search08 = document.getElementById("search-results-div");
-
     search08.innerHTML = "";
 
     const topTrendData = await axios.get("https://music-streaming-api.onrender.com/api/v1/music/get-topSearch");
@@ -175,7 +176,7 @@ async function loadTopTrends() {
     ttH2.innerText = "Top Trending";
 
     topTrend.append(ttH2);
-    search08.appendChild(topTrend);
+    search08.append(topTrend);
 
     const cat01 = document.createElement("div");
     cat01.className = "cat-01";
@@ -189,40 +190,79 @@ function loadSearchSongs(maindiv, data) {
     //main div is search09 and data is array of songs
     const search14 = document.createElement("div");
     search14.className = "search14";
+
     const h214 = document.createElement("h2");
     h214.innerText = "Songs";
 
+    search14.append(h214);
+
     const div14 = document.createElement("div");
+
     const search15 = document.createElement("div");
     search15.className = "search15";
     search15.innerText = "View all";
 
     div14.append(search15);
-    search14.append(h214);
     search14.append(div14);
-
     maindiv.append(search14);
 
     const search16 = document.createElement("div");
     search16.className = "search16";
 
     for (let i = 0; i < data.length; i++) {
-        loadSongList(search16, data[i]);
+        loadSongList(search16, data[i], "search", i);
     }
 
     maindiv.append(search16);
 }
 
-function loadSongList(maindiv, data) {
+function loadSongList(maindiv, data, type, id) {
     const search17 = document.createElement("div");
     search17.className = "search17";
+
     const search18 = document.createElement("div");
     search18.className = "search18";
 
-    const search18_img = document.createElement("img");
-    search18_img.srcset = data.image[2].link;
+    const searchmain = document.createElement("div");
+    searchmain.className = "search27";
 
-    search18.append(search18_img);
+    if (type === "search") {
+        const search18_img = document.createElement("img");
+        search18_img.srcset = data.image[2].link;
+
+        searchmain.append(search18_img);
+    } else {
+        const div1 = document.createElement("div");
+        div1.className = "search22";
+        div1.style.fontSize = "14px";
+        div1.style.fontWeight = "600";
+
+        div1.innerText = id + 1;
+
+        searchmain.append(div1);
+    }
+
+    const hovPlay = document.createElement("div");
+    hovPlay.className = "search25";
+
+    hovPlay.onclick = playCategory.bind({ ...data, type: "song" });
+
+    const search26 = document.createElement("div");
+    search26.className = "search26";
+
+    if (type === "search") {
+        search26.style.backgroundColor = "#000000d3;";
+    }
+
+    const i3 = document.createElement("i");
+    i3.className = "fa-solid fa-play fa-sm";
+    i3.style.color = "white";
+
+    search26.append(i3);
+    hovPlay.append(search26);
+    searchmain.append(hovPlay);
+    search18.append(searchmain);
+    search17.append(search18);
 
     const search24 = document.createElement("div");
     search24.className = "search24";
@@ -234,9 +274,12 @@ function loadSongList(maindiv, data) {
     search20.className = "search20";
 
     const h320 = document.createElement("h3");
-    h320.innerText = data.title;
+    h320.innerText = type === "search" ? data.title : data.name;
+
+    h320.onclick = openDetails.bind({ ...data, type: "song" });
 
     search20.append(h320);
+    search19.append(search20);
 
     const search21 = document.createElement("div");
     search21.className = "search21";
@@ -245,26 +288,23 @@ function loadSongList(maindiv, data) {
     p21.innerText = data.primaryArtists;
 
     search21.append(p21);
-
-    search19.append(search20);
     search19.append(search21);
-
     search24.append(search19);
+    search17.append(search24);
 
     const search22 = document.createElement("div");
     search22.className = "search22";
 
     const i22 = document.createElement("i");
-    i22.className = "fa-regular fa-heart fa-lg";
-
-    i22.style.color = "white";
+    i22.onclick = handleLike.bind({ ...data, type: "song" });
+    if (likedData.song.some((val) => val === data.id)) {
+        i22.className = "fa-solid fa-heart fa-lg";
+    } else {
+        i22.className = "fa-regular fa-heart fa-lg";
+    }
 
     search22.append(i22);
-
-    search17.append(search18);
-    search17.append(search24);
     search17.append(search22);
-
     maindiv.append(search17);
 }
 
@@ -277,14 +317,13 @@ function loadCategory(maindiv, data, type) {
     h223.innerText = type;
 
     h2Div.append(h223);
+    search23.append(h2Div);
 
     const cat01 = document.createElement("div");
     cat01.className = "cat-01";
 
     createListItems(data, cat01, "search");
 
-    search23.append(h2Div);
     search23.append(cat01);
-
     maindiv.append(search23);
 }
